489c7cf8c90ab97fbc5b94bb4fcf18c8daaee734
Samuel Narcisse <narcisse118@gmail.com>
2023-10-16 00:09:07 -0400

    added React Boostrap Grid Layout

--- src/BusinessList.js
+++ src/BusinessList.js
@@ -1,18 +1,35 @@
 import React from "react";
 import Business from './Business'
+import Container from 'react-bootstrap/Container/';
+import Row from 'react-bootstrap/Row/';
+import Col from 'react-bootstrap/Col/';
+import _ from 'lodash';
 
 function BusinessList (){
 
+    //Creating Items 
     const items = []
     for (let index = 0; index < 12; index++) {
          items.push('Business')
     }
+    //Mapping all items as Columns
     const listItems = items.map( item =>  {
-        return ( <Business/>)
+        return ( <Col><Business/></Col>)
     });
 
+    //Splitting Columns into a grid
+    const gridItems = _.chunk(listItems,Math.floor((listItems.length)/3));
 
-    return (<div>{listItems}</div>)
+    const gridComponent = gridItems.map( items => {
+        return (<Row>{items}</Row>);
+    })
+
+
+
+    return (
+    <Container>{gridComponent}
+    </Container>
+    )
 
 }
 
