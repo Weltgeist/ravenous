259ed93ea9a65a51ca6eeb10e3d3354a800c277d
Samuel Narcisse <narcisse118@gmail.com>
2023-10-16 21:13:16 -0400

    worked on responsive layout

--- src/Business.js
+++ src/Business.js
@@ -9,7 +9,7 @@ import Container from 'react-bootstrap/Container';
 
 function Business (){
     return (
-        <Card style={{ width: '16rem' }} className='business'>
+        <Card style={{ width: '18rem' }} className='business'>
             <Card.Img  
             className='business-pic'
             src = {business.src}
@@ -19,16 +19,16 @@ function Business (){
                 <Card.Title className='business-name'>{business.name.toUpperCase()}</Card.Title>
                 <Container fluid className='business-details'>
                 <Row>
-                    <Col width><Card.Text className='business-address'>{business.address}</Card.Text></Col>
-                    <Col><Card.Text className='business-category'>{business.category.toUpperCase()}</Card.Text></Col>
+                    <Col xs={7}  md={7} lg={7} xl={7} xxl={7}><Card.Text className='business-address'>{business.address}</Card.Text></Col>
+                    <Col xs={5} md={5} lg={5} xl={5} xxl={5}><Card.Text className='business-category'>{business.category.toUpperCase()}</Card.Text></Col>
                 </Row>
                 <Row>
-                    <Col ><Card.Text className='business-city'>{business.city}</Card.Text></Col>
-                    <Col><Card.Text className='business-rating'>{business.rating} Star</Card.Text></Col>
+                    <Col xs={7}  md={7} lg={7} xl={7} xxl={7}><Card.Text className='business-city'>{business.city}</Card.Text></Col>
+                    <Col xs={5} md={5} lg={5} xl={5} xxl={5}><Card.Text className='business-rating'>{business.rating} Star</Card.Text></Col>
                 </Row>
                 <Row>
-                    <Col className='business-state-zip'><Card.Text className='business-state'>{business.state} </Card.Text><Card.Text className='business-zipcode'>{business.zipCode}</Card.Text></Col>
-                    <Col><Card.Text className='business-review'>{business.reviewCount} Reviews</Card.Text></Col>
+                    <Col xs={7}  md={7} lg={7} xl={7} xxl={7} className='business-state-zip'><Card.Text className='business-state'>{business.state} </Card.Text><Card.Text className='business-zipcode'>{business.zipCode}</Card.Text></Col>
+                    <Col xs={5} md={5} lg={5} xl={5} xxl={5}><Card.Text className='business-review'>{business.reviewCount} Reviews</Card.Text></Col>
                 </Row>
                 </Container>
             </Card.Body>
