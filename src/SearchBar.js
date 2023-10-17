92fb8a4af3e1877234c981e63e6b7a8714bf632e
Samuel Narcisse <narcisse118@gmail.com>
2023-10-16 22:45:43 -0400

    added content to search bar, used stack to layout

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -3,38 +3,44 @@ import Navbar from 'react-bootstrap/Navbar';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import InputGroup from 'react-bootstrap/InputGroup';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Stack from 'react-bootstrap/Stack';
+import { Container } from 'react-bootstrap';
+import './SearchBar.css'
 
 function SearchBar (){
     return (
-        <Navbar className="bg-body-tertiary justify-content-between">
-          <Form inline>
-            <InputGroup>
-              <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
-              <Form.Control
-                placeholder="Username"
-                aria-label="Username"
-                aria-describedby="basic-addon1"
-              />
-            </InputGroup>
-          </Form>
-          <Form inline>
-            <Row>
-              <Col xs="auto">
-                <Form.Control
-                  type="text"
-                  placeholder="Search"
-                  className=" mr-sm-2"
-                />
-              </Col>
-              <Col xs="auto">
-                <Button type="submit">Submit</Button>
-              </Col>
-            </Row>
-          </Form>
-        </Navbar>
+        <Container className = 'nav-bar-container'>
+            <Navbar className=".bg-transparent  nav-bar-item">{/* justify-content-between */}
+                <Stack gap={3} className='form-stack-container'>
+                    <Stack direction="horizontal" gap={3} className='form-stack-container'>
+                    <Form inline>
+                        <InputGroup>
+                        <Form.Control
+                            type="text"
+                            placeholder="Search Business"
+                            aria-label="Search Business"
+                            aria-describedby="basic-addon1"
+                        />
+                        </InputGroup>
+                    </Form>
+                    <Form inline> 
+                        <InputGroup>
+                            <Form.Control
+                            type="text"
+                            placeholder="Where?"
+                            aria-label="Where?"
+                            className=" mr-sm-2"
+                            />
+                        </InputGroup>
+                    </Form>
+                    </Stack>
+                    <Stack direction="horizontal" gap={3} className='form-stack-container'>
+                        <Button type="submit" className = 'navbar-button'>Submit</Button>
+                    </Stack>
+                </Stack>
+            </Navbar>
+        </Container>
       );
     }
-    
+
 export default SearchBar;
