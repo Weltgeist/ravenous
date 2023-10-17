0df90a2d55bd76eb173019955ffc9d0f500cabef
Samuel Narcisse <narcisse118@gmail.com>
2023-10-16 21:40:33 -0400

    added default search bar to site , react compo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import BusinessList from './BusinessList';
+import SearchBar from './SearchBar'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <SearchBar/>
         <BusinessList/>
       </header>
     </div>
