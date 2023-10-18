7e735e1368a2f380c7ae7c96910b1734ee8e73ad
Samuel Narcisse <narcisse118@gmail.com>
2023-10-18 19:15:10 -0400

    changed business to use props

--- src/Business/Business.js
+++ src/Business/Business.js
@@ -1,34 +1,32 @@
 import React from 'react';
 import './Business.css';
-import business from './BusinessData' ;
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 
-function Business (){
+function Business (props){
     return (
         <Card style={{ width: '19rem' }} className='business'>
             <Card.Img  
             className='business-pic'
-            src = {business.src}
-            alt = {business.alt}
+            src = {props.src}
+            alt = {props.alt}
             />
             <Card.Body>
-                <Card.Title className='business-name'>{business.name.toUpperCase()}</Card.Title>
+                <Card.Title className='business-name'>{props.name.toUpperCase()}</Card.Title>
                 <Container fluid className='business-details'>
                 <Row>
-                    <Col xs={8}  md={8} lg={8} xl={8} xxl={8}><Card.Text className='business-address'>{business.address}</Card.Text></Col>
-                    <Col xs={4} md={4} lg={4} xl={4} xxl={4}><Card.Text className='business-category'>{business.category.toUpperCase()}</Card.Text></Col>
+                    <Col xs={8}  md={8} lg={8} xl={8} xxl={8}><Card.Text className='business-address'>{props.address}</Card.Text></Col>
+                    <Col xs={4} md={4} lg={4} xl={4} xxl={4}><Card.Text className='business-category'>{props.category.toUpperCase()}</Card.Text></Col>
                 </Row>
                 <Row>
-                    <Col xs={8}  md={8} lg={8} xl={8} xxl={8}><Card.Text className='business-city'>{business.city}</Card.Text></Col>
-                    <Col xs={4} md={4} lg={4} xl={4} xxl={4}><Card.Text className='business-rating'>{business.rating} stars</Card.Text></Col>
+                    <Col xs={8}  md={8} lg={8} xl={8} xxl={8}><Card.Text className='business-city'>{props.city}</Card.Text></Col>
+                    <Col xs={4} md={4} lg={4} xl={4} xxl={4}><Card.Text className='business-rating'>{props.rating} stars</Card.Text></Col>
                 </Row>
                 <Row>
-                    <Col xs={8}  md={8} lg={8} xl={8} xxl={8} className='business-state-zip'><Card.Text className='business-state'>{business.state} </Card.Text><Card.Text className='business-zipcode'>{business.zipCode}</Card.Text></Col>
-                    <Col xs={4} md={4} lg={4} xl={4} xxl={4}><Card.Text className='business-review'>{business.reviewCount} reviews</Card.Text></Col>
+                    <Col xs={8}  md={8} lg={8} xl={8} xxl={8} className='business-state-zip'><Card.Text className='business-state'>{props.state} </Card.Text><Card.Text className='business-zipcode'>{props.zipCode}</Card.Text></Col>
+                    <Col xs={4} md={4} lg={4} xl={4} xxl={4}><Card.Text className='business-review'>{props.reviewCount} reviews</Card.Text></Col>
                 </Row>
                 </Container>
             </Card.Body>
