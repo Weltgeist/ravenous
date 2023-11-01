489756685dbde45ce6ab7a538ed713b3de9e4907
Samuel Narcisse <narcisse118@gmail.com>
2023-10-31 21:36:55 -0400

    added outer layer to make nav pic cover

--- src/SearchBar/component/SearchBar.js
+++ src/SearchBar/component/SearchBar.js
@@ -11,48 +11,50 @@ import Nav from 'react-bootstrap/Nav';
 function SearchBar ({onSubmit,onSearch,onLocation,onSelect}){
 
     return (
-        <Container className = 'nav-bar-container'>
-            <Navbar className=".bg-transparent  nav-bar-item">{/* justify-content-between */}
-                <Form inline onSubmit={onSubmit} className="nav-form">
-                    <Stack gap={3} className='form-stack-container'>
-                        <Stack direction="horizontal" gap={3} className='form-stack-container'>
-                            <InputGroup>
-                            <Form.Control
-                                type="text"
-                                placeholder="Search Business"
-                                aria-label="Search Business"
-                                aria-describedby="basic-addon1"
-                                onChange = {onSearch}
-                            />
-                            </InputGroup>
-                            <InputGroup>
+        <div className = 'nav-bar-outer'>
+            <Container className = 'nav-bar-container'>
+                <Navbar className=".bg-transparent  nav-bar-item">{/* justify-content-between */}
+                    <Form inline onSubmit={onSubmit} className="nav-form">
+                        <Stack gap={3} className='form-stack-container'>
+                            <Stack direction="horizontal" gap={3} className='form-stack-container'>
+                                <InputGroup>
                                 <Form.Control
-                                type="text"
-                                placeholder="Where?"
-                                aria-label="Where?"
-                                className=" mr-sm-2"
-                                onChange = {onLocation}
+                                    type="text"
+                                    placeholder="Search Business"
+                                    aria-label="Search Business"
+                                    aria-describedby="basic-addon1"
+                                    onChange = {onSearch}
                                 />
-                            </InputGroup>
+                                </InputGroup>
+                                <InputGroup>
+                                    <Form.Control
+                                    type="text"
+                                    placeholder="Where?"
+                                    aria-label="Where?"
+                                    className=" mr-sm-2"
+                                    onChange = {onLocation}
+                                    />
+                                </InputGroup>
+                            </Stack>
+                            <Stack direction="horizontal" gap={3} className='form-stack-container'>
+                                <Button type="submit" className = 'navbar-button'>Submit</Button>
+                            </Stack>
+                                <Nav fill variant="tabs"  onSelect={onSelect}>
+                                    <Nav.Item>
+                                        <Nav.Link eventKey="best_match">Best Match</Nav.Link>
+                                    </Nav.Item>
+                                    <Nav.Item>
+                                        <Nav.Link eventKey="rating">Highest Rated</Nav.Link>
+                                    </Nav.Item>
+                                    <Nav.Item>
+                                        <Nav.Link eventKey="review_count">Most Reviewed</Nav.Link>
+                                    </Nav.Item>
+                                </Nav>
                         </Stack>
-                        <Stack direction="horizontal" gap={3} className='form-stack-container'>
-                            <Button type="submit" className = 'navbar-button'>Submit</Button>
-                        </Stack>
-                            <Nav fill variant="tabs"  onSelect={onSelect}>
-                                <Nav.Item>
-                                    <Nav.Link eventKey="best_match">Best Match</Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link eventKey="rating">Highest Rated</Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link eventKey="review_count">Most Reviewed</Nav.Link>
-                                </Nav.Item>
-                            </Nav>
-                    </Stack>
-                </Form>
-            </Navbar>
-        </Container>
+                    </Form>
+                </Navbar>
+            </Container>
+        </div>
       );
     }
 
