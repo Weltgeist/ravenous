71549aaa15170b95c6d5ce7966bef2350a9f68f0
Samuel Narcisse <narcisse118@gmail.com>
2023-11-02 23:13:16 -0400

    added effect to show 1st thing when render is 1 search

--- src/App/App.js
+++ src/App/App.js
@@ -4,6 +4,7 @@ import SearchBarContainer from '../SearchBar/container/SearchBarContainer'
 import business from './BusinessData' ;
 import  { useState } from 'react';
 import  { useEffect } from 'react';
+import { YelpApiConnector } from '../utils/Yelp';
 
 function App() {
 
@@ -14,18 +15,39 @@ function App() {
     setsearchData(data);
   }
 
- //Creating Items 
+ //Creating Default Items 
   const businessList = []
   for (let index = 0; index < 12; index++) {
     businessList.push(business)
 }
+
+  function startUpdate() {
+    // start search
+    const search = "africain"
+    const location = "charlotte,NC"
+    const filterOption ="review_count"
+    // Make Yelp Connector
+    const  yelpCall = new YelpApiConnector("http://localhost:3001","/yelp/businesses/search"); //using express server to fix cors issue
+
+    // Get initial Yelp Call
+    const data = yelpCall.getYelpSearch(search,location,filterOption);
+    //Updating App with Yelp Search result.
+    data.then( responseStructured => setsearchData(responseStructured))
+    .catch(err => console.error(err));
+    
+  }
+  // Init call to startUpdate. Makes sure, yelp is called once to get a not empty page. 
+  useEffect(() => {
+    startUpdate()
+  },[])//only run on 1st render.
+  
 
   return (
     <div className="App">
       <header className="App-header">
       <h1 id="site-title">Ravenous</h1>
       <SearchBarContainer onYelpSearch= {handleSearchData}/>
-      <BusinessList businessList = {searchData}/>    
+      <BusinessList businessList = {searchData}/>       
       </header>
     </div>
   );
